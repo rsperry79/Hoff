784032bfd906819a95b8e13ac7d7fbdd4467f359
Richard Sperry <Richard@SperryFamily.net>
2023-12-06 15:09:55 -0800

    Main (#2)
    * Update README.md
    
    * update Master (#1)
    
    * Add DI
    
    * Add MD and gitIgnore
    
    * Change DI Tests to IntegrationTests
    
    * moved old code and integrated
    
    * broken ssd13 display code
    
    * fixed disp test by using real HW
    
    * added sensor and tests
    
    * Added editorconfig, esp32 code, and tests. Cleaned code
    
    * Refactoring
    
    * add tests
    
    * broken
    
    * broken
    
    * move pc
    
    * Major changes
    
    * Updates
    
    * updates
    
    * Updates
    
    * updates
    
    * updates
    
    * add rtc
    
    * updates
    
    * DI changes
    
    * update and add server base
    
    ---------
    
    Co-authored-by: Richard Sperry <rsperry79@hotmail.com>
    
    ---------
    
    Co-authored-by: Richard Sperry <rsperry79@hotmail.com>

---
+++ src/Core/Services/Settings/Hoff.Core.Services.Settings/Settings.cs
@@ -0,0 +1,100 @@
+﻿using System;
+
+using Hoff.Hardware.Common.Interfaces.Storage;
+
+using nanoFramework.Json;
+
+namespace Hoff.Core.Services.Settings
+{
+    public class Settings<T> : IDisposable
+    {
+        #region Fields
+
+        protected static T settings;
+        protected IEeprom Eeprom;
+
+        private bool disposedValue;
+        private byte startLocation = 0x00;
+
+        #endregion Fields
+
+        #region Public Constructors
+
+        public Settings(IEeprom eeprom)
+        {
+            this.Eeprom = eeprom;
+
+            if (settings is null)
+            {
+                this.GetSettings();
+            }
+        }
+
+        #endregion Public Constructors
+
+        #region Properties
+
+        public byte StartLocation
+        {
+            get => this.startLocation;
+            set
+            {
+                if (this.startLocation != value)
+                {
+                    this.startLocation = value;
+                    this.GetSettings();
+                }
+            }
+        }
+
+        #endregion Properties
+
+        #region Public Methods
+
+        public void Dispose()
+        {
+            // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
+            this.Dispose(disposing: true);
+            GC.SuppressFinalize(this);
+        }
+
+        public bool WriteSettings()
+        {
+            string result = JsonConvert.SerializeObject(this);
+            bool write = this.Eeprom.WriteString(this.startLocation, result);
+            return write;
+        }
+
+        #endregion Public Methods
+
+        #region Protected Methods
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!this.disposedValue)
+            {
+                if (disposing)
+                {
+                    this.WriteSettings();
+                }
+
+                this.disposedValue = true;
+            }
+        }
+
+        protected void GetSettings()
+        {
+            try
+            {
+                string data = this.Eeprom.ReadString(this.startLocation);
+                T temp = (T)JsonConvert.DeserializeObject(data, typeof(T));
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        #endregion Protected Methods
+    }
+}
